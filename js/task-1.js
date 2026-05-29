55b2bfa471458dd31e123cf4789cb3230e86ea60
imyroslav <imyroslav@ukr.net>
2026-05-28 21:40:03 -0300

    task-1 done

---
+++ js/task-1.js
@@ -0,0 +1,19 @@
+
+
+function slugify(title) {
+
+    const titleLow =  title.toLowerCase();
+
+    const words = titleLow.split(" ")
+
+    const slug = words.join("-");
+
+    return slug;
+
+
+};
+
+console.log(slugify("Arrays for beginners"));
+console.log(slugify("English for developers"));
+console.log(slugify("Ten secrets of JavaScript"));
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
